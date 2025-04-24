1ca35c588ecb5f4bcd454689f312f10d5f6ae830
VadanKhan <vadankhan1@gmail.com>
2025-04-24 10:58:45 +0100

    first commit

---
+++ vadankhan_spectrumanalysis.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "D:\Tester 58174\Output\vadankhan-spectrumanalyser-deploybuild"
+call .venv\Scripts\activate.bat
+python vadankhan_spectrumanalyser_deploybuild\spectrum_files_transform_watchdog.py
