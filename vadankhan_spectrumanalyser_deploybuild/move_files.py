06e34d2478848c85d57473f00ebf9efc84b49df6
VadanKhan <vadankhan1@gmail.com>
2025-05-12 09:28:04 +0100

    move files investigation

--- vadankhan_spectrumanalyser_deploybuild/move_files.py
+++ vadankhan_spectrumanalyser_deploybuild/move_files.py
@@ -1,10 +1,12 @@
 import os
 import shutil
 
-source_folder = r"C:\Path\To\Source"
-destination_folder = r"C:\Path\To\Destination"
+source_folder = r"C:\Users\762093\Documents\deployment-emulator-spectrumanalyser\Output\vadankhan-spectrumanalyser-deploybuild\PROCESSED_SPECTRA"
+destination_folder = r"J:\RND\Vadan Khan\PROCESSED_SPECTRA_SMSR"
 
 for filename in os.listdir(source_folder):
+    if filename == "spectrum_analyser_log.txt":
+        continue  # Skip this specific file
     src_file = os.path.join(source_folder, filename)
     dst_file = os.path.join(destination_folder, filename)
     shutil.move(src_file, dst_file)
---
+++ vadankhan_spectrumanalysis_folderclear.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\762093\Documents\deployment-emulator-spectrumanalyser\Output\vadankhan-spectrumanalyser-deploybuild"
+start /min cmd /c "python vadankhan_spectrumanalyser_deploybuild\move_files.py"
